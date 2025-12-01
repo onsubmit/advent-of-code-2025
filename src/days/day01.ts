962787f1752b4dbb1defd9b6cf47316248c521f2
Andy <onsubmit@outlook.com>
2025-11-30 18:10:20 -0800

    Remove old day 1 implementation

--- src/days/day01.ts
+++ src/days/day01.ts
@@ -1,25 +1,13 @@
-import { sortArray, sumArrayBy } from '../arrayMethods';
-
 export const getPartOneSolution = (input: string): string => {
-  const { left, right } = parseInput(input);
-  return sumArrayBy(left, (l, i) => Math.abs(l - right[i])).toString();
+  return '';
 };
 
 export const getPartTwoSolution = (input: string): string => {
-  const { left, right } = parseInput(input);
-  return sumArrayBy(left, (l) => l * right.filter((r) => r === l).length).toString();
+  return '';
 };
 
-const parseInput = (input: string): { left: Array<number>; right: Array<number> } => {
+const parseInput = (input: string): any => {
   const lines = input.split('\n').filter(Boolean);
 
-  const left: Array<number> = [];
-  const right: Array<number> = [];
-  for (const line of lines) {
-    const split = line.split(' ').filter(Boolean);
-    left.push(parseInt(split[0], 10));
-    right.push(parseInt(split[1], 10));
-  }
-
-  return { left: sortArray(left), right: sortArray(right) };
+  return lines;
 };
